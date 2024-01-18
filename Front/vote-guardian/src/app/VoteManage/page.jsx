719161ac3f807299b3b297cee4f35b77960b8b8e
ssubing <5515983@naver.com>
2024-01-18 17:56:19 +0900

    [✨feat] 로그인 후 메인페이지에 있는 메뉴바 생성(VOTEGUARD-10)
    - 각각의 메뉴를 눌렀을 때 드롭다운이 되는 것까지 구현함
    - 드롭다운할 때 효과를 추가할 예정
    - 아직 반응형은 미구현이며 최종적으로 모바일과 pc에서 다른 형태의 메뉴바를 표시하고자 함

---
+++ Front/vote-guardian/src/app/VoteManage/page.jsx
@@ -0,0 +1,12 @@
+import MenuBar from "@/components/VoteManageMenu/MenuBar"
+import './VoteManage.scss'
+
+export default function VoteManage() {
+    return(
+        <div className="flex-wrap">
+            <div className="wrap">
+                <MenuBar></MenuBar>
+            </div>
+        </div>
+    )
+}
