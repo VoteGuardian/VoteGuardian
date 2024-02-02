29cb448dda657603e338e4169c23420b70e698a4
ssubing <5515983@naver.com>
2024-02-02 16:15:04 +0900

    [💄design] 페이지네이션 CSS 설정(VOTEGUARD-37)
    - 현재 보고 있는 페이지의 버튼이 한 눈에 들어오도록 색을 지정
    - 다른 페이지 버튼에 마우스를 올렸을 때 배경색을 현재 페이지 버튼보다 연하게 설정

--- Front/vote-guardian/src/components/common/Pagiantion/Pagination.jsx
+++ Front/vote-guardian/src/components/common/Pagiantion/Pagination.jsx
@@ -4,75 +4,111 @@ import { GrCaretNext } from "react-icons/gr";
 import './Pagination.scss';
 import { useRecoilState } from "recoil";
 import { pageNumState } from "@/recoil/atoms/createVoteListState";
+import { useEffect, useState } from "react";
 
 export default function Pagination() {
-    const [currentPage, setCurrentPage] = useRecoilState(pageNumState);
-    const limitPageCount = 5; //5개 단위로 페이지를 끊을 것
-    //다른 데서 받았을 totalPageCount
-    const totalPageCount = 8;//더미    
+    //현재 페이지 번호
+    const [currentPage, setCurrentPage] = useState(1);
+    //페이지를 몇 개 단위로 묶을지에 대한 수
+    const limitPageCount = 5; 
+    //api 요청으로 받을 전체 페이지 수(현재는 더미데이터)
+    const totalPageCount = 8;   
 
-    //페이지를 5개씩 묶은 그룹 배열 생성하기
+    //페이지를 5개(limitPageCount)씩 묶은 그룹 배열 생성하기
     const createPageGroupList = (totalPageCount, currentPage) => {
-        //페이지 번호를 담을 전체 그룹
+        //페이지 번호 전체(1부터 totalPageCount까지)를 담을 배열
         const totalPageGroup = Array(totalPageCount).fill(currentPage).map((x, y) => x + y);
-        //5페이지 단위로 그룹 나누기
+        //5페이지(limitPageCount) 단위로 그룹 나누기
         const pageGroupList = [];
         for(let i = 0; i < totalPageGroup.length; i += limitPageCount) {
             pageGroupList.push(totalPageGroup.slice(i, i+limitPageCount));
         }
         return pageGroupList;
     }
+    //페이지 그룹 생성
+    let pageGroup = createPageGroupList(totalPageCount, 1);
+    //페이지 그룹의 마지막 번호
+    const pageLastGroup = pageGroup.length;
     
     //현재 페이지가 속한 그룹 구하기
     const getCurrentPageGroup = (currentPage, limitPageCount) => {
         return Math.ceil(currentPage/limitPageCount)-1;
     }
-
-    let pageGroup = createPageGroupList(totalPageCount, 1);
-    //페이지 그룹의 마지막 번호
-    const pageLastGroup = pageGroup.length;
+    
     //현재 페이지가 속한 그룹(페이지가 바뀌면 같이 바뀜)
     let currentGroup = getCurrentPageGroup(currentPage, limitPageCount);
+
+    //페이지가 바뀔 때마다 실행
+    useEffect(() => {
+        //초기 페이지 버튼색 설정
+        //페이지 버튼들을 하나의 배열로 만들기
+        const buttonList = document.getElementsByClassName("page-button-list");
+        const buttonListChild = buttonList[0].childNodes;
+        //마지막 번호까지
+        let lastButtonNumber = buttonListChild.length;
+        //현재 버튼이 배열에서 몇 번째인지
+        let currentButton = 0;
+        //현재 보고 있는 페이지의 버튼은 클래스 이름에 clicked 추가
+        if(currentPage % 5 === 0) {
+            currentButton = currentPage-1;
+            buttonListChild[currentButton].classList.add("clicked");
+            buttonListChild[currentButton].classList.remove("none");
+        } else {
+            currentButton = (currentPage%5)-1;
+            buttonListChild[currentButton].classList.add("clicked");
+            buttonListChild[currentButton].classList.remove("none");
+        }
+        //현재 페이지를 제외한 번호들에서 clicked 제거(css 효과 제거를 위함)
+        for(let i = 0; i < lastButtonNumber; i++) {
+            if(i !== currentButton) {
+                buttonListChild[i].classList.remove("clicked");
+                buttonListChild[i].classList.add("none");
+            }
+        }
+    }, [currentPage]);
     
-    //이전 버튼 클릭 시
-    //let previousPageGroup = 0;
+    //이전 버튼 클릭 시(페이지 그룹 이동)
     function handlePreviousPage() {
         //이동할 페이지는 이전 페이지 그룹의 마지막 숫자
         let previousPageGroup = currentGroup-1;
-        setCurrentPage(pageGroup[previousPageGroup][4]);
+        setCurrentPage(pageGroup[previousPageGroup][4]); //현재 페이지 변경
+        //현재 페이지가 속한 그룹도 변경됨
         currentGroup = getCurrentPageGroup(currentPage, limitPageCount);
     }
 
-    //다음 버튼 클릭 시
-    //let nextPageGroup = 0;
+    //다음 버튼 클릭 시(페이지 그룹 이동)
     function handleNextPage() {
         //이동할 페이지는 다음 페이지 그룹의 첫 번째 숫자
         let nextPageGroup = currentGroup+1;
-        setCurrentPage(pageGroup[currentGroup+1][0]);
+        setCurrentPage(pageGroup[nextPageGroup][0]); //현재 페이지 변경
+        //현재 페이지가 속한 그룹도 변경됨
+        currentGroup = getCurrentPageGroup(currentPage, limitPageCount);
     }
 
     //페이지 버튼 클릭 시 url 변경
-    function handleChangePage(page) {
-
+    function handleChangePage(e) {
+        //현재 페이지 변경
+        setCurrentPage(e.target.innerText);
     }
+
     return (
         <div className="pagenation-wrap">
             {currentPage > 5 &&
                 <div className="prev-button-wrap" onClick={handlePreviousPage}>
                     <GrCaretPrevious />
-                    <p>이전</p>
                 </div>
             }
-            {pageGroup[currentGroup] && pageGroup[currentGroup].map((n) => {
-                return(
-                    <div>
-                        <p>{n}</p>
-                    </div>
-                )
-            })}
+            <div className="page-button-list">
+                {pageGroup[currentGroup] && pageGroup[currentGroup].map((n) => {
+                    return(
+                        <div className="page-button-wrap" onClick={handleChangePage}>
+                            <p className="page-button">{n}</p>
+                        </div>
+                    )
+                })}
+            </div>
             {currentGroup !== pageLastGroup-1 &&
                 <div className="next-button-wrap" onClick={handleNextPage}>
-                    <p>다음</p>
                     <GrCaretNext />
                 </div>
             }
