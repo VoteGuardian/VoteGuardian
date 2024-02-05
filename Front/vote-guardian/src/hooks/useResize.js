a10d136c0997f02c2fffdd740c78b43abdbc7303
ssubing <5515983@naver.com>
2024-02-05 15:09:32 +0900

    [✨] Tablet 화면 추가(VOTEGUARD-27)
    - 기존에는 화면을 Mobile과 PC 두 가지의 경우로 나누었으나 Tablet일 때의 경우를 새로 추가

--- Front/vote-guardian/src/hooks/useResize.js
+++ Front/vote-guardian/src/hooks/useResize.js
@@ -1,13 +1,18 @@
 import { useMediaQuery } from "react-responsive";
 
 export const Mobile = ({children}) => {
-    const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+    const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
 
     return <>{isMobile && children}</>
 }
+export const Tablet = ({children}) => {
+    const isTablet = useMediaQuery({ query: "(min-width:768px) and (max-width:1600px)" });
+
+    return <>{isTablet && children}</>
+}
 
 export const PC = ({children}) => {
-    const isPc = useMediaQuery({ query: "(min-width: 769px)" });
+    const isPc = useMediaQuery({ query: "(min-width: 1601px)" });
 
     return <>{isPc && children}</>
 }
