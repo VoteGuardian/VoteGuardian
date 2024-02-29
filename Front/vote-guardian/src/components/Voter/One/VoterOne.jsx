06db4ff0bb1d61eb7e823b2eddd329ec16d590ba
ssubing <5515983@naver.com>
2024-02-29 14:01:03 +0900

    [✨feat] 투표자 이름, 이메일 유효성 검사 구현(VOTEGUARD-66)
    - 투표자를 한 명 등록할 때 이메일 유효성 검사를 통해 이메일의 형태가 맞는 경우에만 등록할 수 있도록 구현
    - 투표자 이름은 조건(30자 이내)에 맞을 때만 등록할 수 있도록 구현

--- Front/vote-guardian/src/components/Voter/One/VoterOne.jsx
+++ Front/vote-guardian/src/components/Voter/One/VoterOne.jsx
@@ -1,14 +1,19 @@
+import './VoterOne.scss'
 import Alert from "@/components/Common/Alert/Alert";
 import Button from "@/components/Common/Button/Button";
 import { voterListState, voterOneState } from "@/recoil/atoms/voteAtoms";
 import { useState } from "react";
-import { useRecoilState } from "recoil";
+import { useRecoilState, useSetRecoilState } from "recoil";
 
 export default function VoterOne() {
-    const [voterOne, setVoterOne] = useRecoilState(voterOneState);
+    const setVoterOne = useSetRecoilState(voterOneState);
     const [voterList, setVoterList] = useRecoilState(voterListState);
+    //이름, 이메일
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
+    //이름과 이메일 유효성 확인
+    const [nameValid, setNameValid] = useState(false);
+    const [emailValid, setEmailValid] = useState(false);
 
     //알람창
     const [alert, setAlert] = useState(false);
@@ -16,21 +21,32 @@ export default function VoterOne() {
 
     //투표자 한 명 이름
     function handleVoterOneName(e) {
-        if(e.target.value.length > 30) {
-
+        //이름은 30자 이내로 작성
+        if(e.target.value.length > 30) document.getElementsByClassName("name-valid")[0].classList.add('show');
+        else {
+            document.getElementsByClassName("name-valid")[0].classList.remove('show');
+            setNameValid(true);
+            setName(e.target.value);
         }
-        else setName(e.target.value);
     }
     //투표자 한 명 이메일
     function handleVoterOneEmail(e) {
-        //이메일 조건 확인
-        setEmail(e.target.value);
+        //이메일 유효성 검사
+        const newEmail = e.target.value;
+
+        const validateEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
+        if(!validateEmail.test(newEmail)) document.getElementsByClassName("email-valid")[0].classList.add('show');
+        else {
+            document.getElementsByClassName("email-valid")[0].classList.remove('show');
+            setEmailValid(true)
+            setEmail(e.target.value);
+        }
     }
     //투표자 한 명 등록
     function handleVoterOneAdd() {
         //이름과 이메일 모두 작성되어야 함
         let voterIs = false;
-        if(name !== '' && email !== '') {
+        if(name !== '' && email !== '' && nameValid && emailValid) {
             if(voterList.length > 0) {
                 for(let i = 0; i < voterList.length; i++) {
                     //이미 등록된 사람이라면
@@ -82,10 +98,12 @@ export default function VoterOne() {
             <div className='vote-one-wrap'>
                 <label for="voterName">이름</label>
                 <input onInput={handleVoterOneName} type="text" id="voterName" placeholder='이름을 작성해주세요'></input>
+                <p className="name-valid">이름은 30자 이내로 작성해주세요</p>
             </div>
             <div className='vote-one-wrap'>
                 <label for="voterName">이메일</label>
                 <input onInput={handleVoterOneEmail} type="text" id="voterName" placeholder='이메일을 작성해주세요'></input>
+                <p className="email-valid">유효하지 않은 이메일 양식입니다.</p>
             </div>
             <div className="button-group-flex">
                 <div onClick={handleVoterCancle}>
